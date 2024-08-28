cc98f6d00bd6cfff8bef4a9f37c8fb11c1bd976c
Alimou224 <mamadoualimoubalde129@gmail.com>
2024-08-28 12:22:28 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,14 +11,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
+global.ownernomer = "224626970221"
 global.ownername = "🩵 𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
 global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
 
-global.ownernumber = '22898133388'  //creator number
-global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
+global.ownernumber = '224626970221'  //creator number
+global.ownername = '𓆩꧁𝕊𝔼ℂ𝕂𒆜𝐃𝐀𝐑𝐊꧂𓆪' //owner name
 global.botname = '𝙿𝙰𝚁𝙺𝚈-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
